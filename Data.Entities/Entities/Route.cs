ef9d6c342e5945979e3b1889a98dc1b523af0ad4
Alexandru Miron <mironalex96@gmail.com>
2017-12-12 00:35:03 +0200

    Pushed builded stuff?

--- Data.Entities/Entities/Route.cs
+++ Data.Entities/Entities/Route.cs
@@ -15,7 +15,7 @@ namespace Data.Domain.Entities
         public int OriginStationCode { get; set; }
         public int DestinationStationCode { get; set; }
         public string Type { get; set; }
-        List<RouteNode> RouteNodes { get; set; }
+        public List<RouteNode> RouteNodes { get; set; }
 
     }
 }
