9caeeb13f5b708d44c70d0017fda56db168071ec
StefanCosminR <stefancosmin.romanescu@gmail.com>
2017-12-20 17:53:25 +0200

    Created generic repository interface

---
+++ Data.Entities/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Domain.Interfaces
+{
+    public interface IRepository<T>
+    {
+        IReadOnlyList<T> GetAll();
+        void Create(T entity);
+        void Delete(Guid entityId);
+        void Edit(T entity);
+        T GetById(Guid entityId);
+    }
+}
