d95dc08ff7ad830fccfb61414121e041a49ebef0
StefanCosminR <stefancosmin.romanescu@gmail.com>
2017-12-21 07:42:33 +0200

    Added set method to IDatabaseContext

--- Data.Interfaces/IDatabaseContext.cs
+++ Data.Interfaces/IDatabaseContext.cs
@@ -6,6 +6,7 @@ namespace Data.Persistance
     public interface IDatabaseContext
     {
         int SaveChanges();
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
         DbSet<Data.Domain.Entities.Ticket> Tickets { get; set; }
         DbSet<Data.Domain.Entities.Train> Trains { get; set; }
         DbSet<Data.Domain.Entities.Station> Stations { get; set; }
