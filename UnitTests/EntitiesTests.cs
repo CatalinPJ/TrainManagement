24657dbaa9b6e53edfc08c63f09124d2139df93f
AncaGrigoriu <grigoriu.anca2012@gmail.com>
2018-01-10 19:32:15 +0200

    Unit tests

---
+++ UnitTests/EntitiesTests.cs
@@ -0,0 +1,37 @@
+using Data.Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace UnitTests
+{
+    public class EntitiesTests
+    {
+        [Fact]
+        public void Given_Train_Then_IdShouldNotBeNull()
+        {
+            var train = new Train();
+            train.Id.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public void Given_Station_Then_IdShouldNotBeNull()
+        {
+            var station = new Station();
+            station.Id.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public void Given_Ticket_Then_IdShouldNotBeNull()
+        {
+            var ticket = new Ticket();
+            ticket.Id.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public void Given_RouteNode_Then_IdShouldNotBeNull()
+        {
+            var routeNode = new RouteNode();
+            routeNode.Id.Should().NotBeEmpty();
+        }
+    }
+}
