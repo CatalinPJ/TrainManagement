74252246734513c12162821b6abf3c33403f3aed
Unknown <anghelinaelena96@gmail.com>
2018-01-14 21:10:31 +0200

    Create price computer test

---
+++ UnitTests/PriceTest.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Xunit;
+using Data.Domain.Entities;
+using Services;
+
+namespace UnitTests
+{
+    class PriceTest
+    {
+        [Fact]
+        public void Given_Ticket_ResultPrice_ShouldBe_TheSame()
+        {
+            var ticket = new Ticket();
+            var result = new PriceComputer();
+            ticket.Adults = 2;
+            ticket.Km = 400;
+            ticket.Children = 1;
+            ticket.Pet = 1;
+            ticket.TrainCategory = "IR";
+            ticket.Class = 1;
+            var price = 323.4f;
+            var resultprice = result.GetPrice(ticket);
+            resultprice.ShouldBeEquivalentTo(price);
+        }
+    }
+}
